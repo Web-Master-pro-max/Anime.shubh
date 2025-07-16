a15f14d341e89fe5106efd9bd8fc9c552e40b059
Web-Master-pro-max <rockstargammer586@gmail.com>
2025-07-16 20:01:34 +0530

    main.js

--- main.js
+++ main.js
@@ -21,64 +21,141 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
   });
 
-  var swiper = new Swiper(".anime-slider", {
-    slidesPerView: 4,
+  
+
+
+var swiper = new Swiper(".anime-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
+        delay: 4500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true
-  });
+    loop: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
 
-
-  var swiper = new Swiper(".action-slider", {
-    slidesPerView: 4,
+// Apply similar breakpoints to .action-slider, .child-slider, and .family-slider
+// For action-slider
+var swiper = new Swiper(".action-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
+        delay: 3500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true
-  });
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
 
-
-  var swiper = new Swiper(".child-slider", {
-    slidesPerView: 4,
+// For child-slider
+var swiper = new Swiper(".child-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true
-  });
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
 
-  var swiper = new Swiper(".family-slider", {
-    slidesPerView: 4,
+// For family-slider (Hollywood and Horror movies)
+var swiper = new Swiper(".family-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 1500,
-      disableOnInteraction: false,
+        delay: 1500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true
-  });
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
