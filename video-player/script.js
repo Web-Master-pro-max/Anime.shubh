af9c736a1c55539a0f85c18690c6e4e498edc4db
Web-Master-pro-max <rockstargammer586@gmail.com>
2026-08-01 16:38:15 +0530

    dfs

--- video-player/script.js
+++ video-player/script.js
@@ -9,7 +9,7 @@ window.addEventListener('unhandledrejection', function (e) {
 
 document.addEventListener('DOMContentLoaded', async function () {
   try {
-     const EC2_BACKEND_URL = 'http://13.61.84.121:8000';
+     const EC2_BACKEND_URL = 'http://13.202.95.5:8000';
     const API_BASE = window.location.hostname.includes('vercel.app') ? `${EC2_BACKEND_URL}/api` : '/api';
 
     // Get episode ID from URL params
